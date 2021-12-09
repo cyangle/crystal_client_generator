a06e5f25b875bcb2211764fbf45a5270a80d967e
Chao Yang <cyang05@outlook.com>
2021-12-09 19:48:53 +0000

    Refactor script generate_from_env.sh to run in container

--- bin/generate_from_env.sh
+++ bin/generate_from_env.sh
@@ -6,11 +6,15 @@ cd "$BASEDIR/.."
 
 echo "Running in folder: ${PWD}"
 
-[[ -z "${shard_version}" ]] && echo "shard_version is not set" && exit 1
-[[ -z "${out_dir}" ]] && echo "out_dir is not set" && exit 1
-[[ -z "${spec_file_path}" ]] && echo "spec_file_path is not set" && exit 1
-[[ -z "${additional_properties}" ]] && echo "additional_properties is not set" && exit 1
-[[ -z "${image}" ]] && echo "image is not set" && exit 1
+[ -z "${shard_version}" ] && echo "shard_version is not set" && exit 1
+[ -z "${out_dir}" ] && echo "out_dir is not set" && exit 1
+[ -z "${spec_file_path}" ] && echo "spec_file_path is not set" && exit 1
+[ -z "${additional_properties}" ] && echo "additional_properties is not set" && exit 1
+[ -z "${image}" ] && echo "image is not set" && exit 1
+
+user=$(id -u)
+repo_dir=${repo_dir:-$PWD}
+echo "repo_dir is ${repo_dir}"
 
 echo "shard version is ${shard_version}"
 
@@ -19,7 +23,7 @@ rm -rf $out_dir
 
 echo "Generate code for spec file: $spec_file_path"
 echo "Using image $image"
-docker run --rm -v "${PWD}:/gen" --workdir "/gen" $image generate \
+docker run --rm -v "${repo_dir}:/gen" --user "$user:$user" --workdir "/gen" $image generate \
     -g crystal \
     -c crystal_client_config.yml \
     -i $spec_file_path \
