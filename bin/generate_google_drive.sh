62ead98f904cc4ab24d7d3ca51603eb55a911717
Chao Yang <cyang05@outlook.com>
2022-01-23 16:40:16 -0600

    Fix query_params in api to include Array(String)

--- bin/generate_google_drive.sh
+++ bin/generate_google_drive.sh
@@ -4,7 +4,7 @@ BASEDIR=$(dirname "$0")
 
 cd "$BASEDIR/.."
 
-export shard_version=${shard_version:-3.1.2}
+export shard_version=${shard_version:-3.1.4}
 export image=${image:-'openapitools/openapi-generator-cli:latest'}
 export spec_file_path='example_api_specs/google-drive-api-v3-oas3.json'
 export out_dir='out/crystal/google_drive_latest'
