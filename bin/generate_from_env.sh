612e8e0278909ad61c301f1c0277652e12addc46
Chao Yang <cyang05@outlook.com>
2022-06-04 03:19:59 +0000

    Add skip_form_model option to generation scripts

--- bin/generate_from_env.sh
+++ bin/generate_from_env.sh
@@ -11,6 +11,7 @@ echo "Running in folder: ${PWD}"
 [ -z "${spec_file_path}" ] && echo "spec_file_path is not set" && exit 1
 [ -z "${additional_properties}" ] && echo "additional_properties is not set" && exit 1
 [ -z "${image}" ] && echo "image is not set" && exit 1
+[ -z "${skip_form_model}" ] && echo "skip_form_model is not set" && exit 1
 
 user=$(id -u)
 repo_dir=${repo_dir:-$PWD}
@@ -26,7 +27,7 @@ rm -rf $out_dir
 echo "Generate code for spec file: $spec_file_path"
 echo "Using image $image"
 docker run --rm -it -v "${repo_parent_dir}:/gen" --user "$user:$user" --workdir "/gen/crystal_client_generator" $image generate \
-    --global-property skipFormModel=false \
+    --global-property skipFormModel=${skip_form_model} \
     -g crystal \
     -c crystal_client_config.yml \
     -i $spec_file_path \
