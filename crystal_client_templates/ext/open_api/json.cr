4c6b6247e20b579085504136788b4ef3afcd2ae6
Chao Yang <cyang05@outlook.com>
2022-09-29 03:34:48 +0000

    Refactor OpenApi::Json

--- crystal_client_templates/ext/open_api/json.cr
+++ crystal_client_templates/ext/open_api/json.cr
@@ -1,28 +1,26 @@
 module OpenApi
   module Json
-    macro included
-      # Builds the object from a JSON::Any hash
-      # @param [Hash] JSON::Any
-      # @return [Object] Returns the model itself
-      def self.build_from_hash(hash : JSON::Any)
-        from_json(hash.to_json)
-      end
+    # Builds the object from a JSON::Any hash
+    # @param [Hash] JSON::Any
+    # @return [Object] Returns the model itself
+    def self.build_from_hash(hash : JSON::Any)
+      from_json(hash.to_json)
+    end
 
-      # Returns the object in the form of hash
-      # @return [Hash] Returns the object in the form of hash
-      def to_hash : JSON::Any
-        JSON.parse(to_json)
-      end
+    # Returns the object in the form of hash
+    # @return [Hash] Returns the object in the form of hash
+    def to_hash : JSON::Any
+      JSON.parse(to_json)
+    end
 
-      # @see the `==` method
-      # @param [Object] Object to be compared
-      def eql?(o : self)
-        self == o
-      end
+    # @see the `==` method
+    # @param [Object] Object to be compared
+    def eql?(o : self)
+      self == o
+    end
 
-      def after_initialize
-        raise JSON::ParseException.new("Validation failed", 0, 0) if !valid?
-      end
+    def after_initialize
+      raise JSON::ParseException.new("Validation failed", 0, 0) if !valid?
     end
   end
 end
