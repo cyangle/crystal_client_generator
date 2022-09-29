e0c2fac64af72796ada4fcb53b4f318c4bbf9421
Chao Yang <cyang05@outlook.com>
2022-09-29 06:02:33 +0000

    Refactor OpenApi::EnumValidator

--- crystal_client_templates/validators/enum_validator.cr
+++ crystal_client_templates/validators/enum_validator.cr
@@ -1,48 +1,29 @@
 module OpenApi
   class EnumValidator
-    getter name : String
-    getter datatype : String
-    getter allowable_values : Set(String | Int32 | Int64 | Float32 | Float64)
-    getter error_message : String
+    ERROR_MESSAGE = "invalid value for \"%s\", must be one of %s."
 
-    def initialize(name, datatype, allowable_values)
-      @name = name
-      @datatype = datatype
-      @error_message = "invalid value for \"#{name}\", must be one of #{allowable_values.to_a}."
-      @allowable_values = allowable_values.map do |value|
-        case datatype.to_s
-        when /Int64/i
-          value.to_i64
-        when /Int32/i
-          value.to_i
-        when /Float32/i
-          value.to_f32
-        when /Float64/i
-          value.to_f64
-        else
-          value
-        end
-      end.to_set
+    def self.error_message(name : String, allowable_values : StaticArray(T, N)) : String forall T
+      ERROR_MESSAGE % [name, allowable_values.to_s]
     end
 
-    def valid?(value, allow_nil = true)
+    def self.valid?(name : String, value : T?, allowable_values : StaticArray(T, N), allow_nil : Bool = true) : Bool forall T
       return true if allow_nil && value.nil?
 
       !value.nil? && allowable_values.includes?(value.not_nil!)
     end
 
-    def valid!(value, allow_nil = true)
-      raise ArgumentError.new(error_message) unless valid?(value, allow_nil)
-    end
-
-    def all_valid?(values, allow_nil = true)
+    def self.valid?(name : String, values : Array(T)?, allowable_values : StaticArray(T, N), allow_nil : Bool = true) : Bool forall T
       return true if allow_nil && values.nil?
 
       !values.nil? && values.not_nil!.all? { |value| allowable_values.includes?(value) }
     end
 
-    def all_valid!(values, allow_nil = true)
-      raise ArgumentError.new(error_message) unless all_valid?(values, allow_nil)
+    def self.validate(name : String, value : T?, allowable_values : StaticArray(T, N), allow_nil : Bool = true) : Nil forall T
+      raise ArgumentError.new(ERROR_MESSAGE % [name, allowable_values.to_s]) unless valid?(name, value, allowable_values, allow_nil)
+    end
+
+    def self.validate(name : String, values : Array(T)?, allowable_values : StaticArray(T, N), allow_nil : Bool = true) : Nil forall T
+      raise ArgumentError.new(ERROR_MESSAGE % [name, allowable_values.to_s]) unless valid?(name, values, allowable_values, allow_nil)
     end
   end
 end
